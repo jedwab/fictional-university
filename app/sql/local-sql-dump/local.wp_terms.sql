df01b4a942479c858665a5e77307c8b5c3c952ce
Justyna Bobrowska <justynabobrowska88@gmail.com>
2019-02-28 12:39:18 +0100

    start working on custom search route

--- app/sql/local-sql-dump/local.wp_terms.sql
+++
@@ -1,9 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_terms` VALUES
-(1,"Uncategorized","uncategorized",0),
-(2,"Menu 1","menu-1",0),
-(4,"My Footer 1","my-footer-1",0),
-(5,"My Footer 2","my-footer-2",0),
-(6,"award","award",0);
